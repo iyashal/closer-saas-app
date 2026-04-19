39857b1804d6675f4fc04ae147054972683aee40
Yashal <yashal.siddiq@gmail.com>
2026-04-19 18:28:35 -0500

    fix: remove Content-Type header from DELETE bot request

--- apps/web/src/lib/api.ts
+++ apps/web/src/lib/api.ts
@@ -13,7 +13,7 @@ async function request<T>(path: string, init: RequestInit = {}): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
     ...init,
     headers: {
-      'Content-Type': 'application/json',
+      ...(init.body ? { 'Content-Type': 'application/json' } : {}),
       ...authHeaders,
       ...init.headers,
     },
